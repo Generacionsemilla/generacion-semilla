da20bc1ae611d7a3b5c65656ea154f6afbac6cfe
alexbaico <neumann99>
2022-01-12 13:02:12 -0300

    desktop about styles, added fonts and translationable texts

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,14 +9,23 @@ import mid_top from '../assets/about/mid_top.svg';
 import mid_bottom from '../assets/about/mid_bottom.svg';
 import bottom from '../assets/about/bottom.svg';
 import {STYLES_COMMON} from "../assets/styles_common";
+import youtube from "../assets/footer/youtube.png";
+import instagram from "../assets/footer/instagram.png";
+import facebook from "../assets/footer/facebook.png";
+import linkedin from "../assets/footer/linkedin.png";
+import {useTranslation} from "react-i18next";
 
 const About = () => {
+
+    const { t } = useTranslation();
 
     const styles = {
         title:{
             fontFamily: 'tomarik-brush,sans-serif',
-            fontWeight: '400',
-            fontStyle: 'normal'
+            fontWeight: '700',
+            fontStyle: 'normal',
+            color: STYLES_COMMON.colors.azulita,
+            fontSize: '60px'
         },
         top_right:{
             marginTop: '100px',
@@ -26,23 +35,61 @@ const About = () => {
             position: 'absolute',
             top: '37%',
             left: '10%',
-            width: '20%'
+            width: '20%',
+            fontFamily: 'filson-pro,sans-serif',
+            fontWeight: '400',
+            fontStyle: 'normal'
         },
         top_left_text_2:{
             position: 'absolute',
             top: '68%',
             left: '11%',
-            width: '20%'
+            width: '20%',
+            fontFamily: 'filson-pro,sans-serif',
+            fontWeight: '400',
+            fontStyle: 'normal',
+            color: STYLES_COMMON.colors.blancoHielo
         },
         middle_title:{
             fontFamily: 'filson-pro,sans-serif',
             fontWeight: '700',
             fontStyle: 'normal'
         },
+        middle_top_text:{
+            fontFamily: 'filson-pro,sans-serif',
+            fontWeight: '400',
+            fontStyle: 'normal'
+        },
         bottom:{
             marginBottom: '-200px',
             width: '110%',
             marginLeft: '-75px'
+        },
+        icon:{
+            maxWidth: '80px',
+            width: '100%'
+        },
+        middle_bottom_socials_row:{
+            position: 'absolute',
+            top: '45%'
+        },
+        middle_bottom_text_row:{
+            position: 'absolute',
+            top: '62%',
+            fontFamily: 'filson-pro,sans-serif',
+            fontWeight: '400',
+            fontStyle: 'normal',
+            color: STYLES_COMMON.colors.blancoHielo
+        },
+        middle_bottom_image:{
+            width: '120%',
+            marginTop: '-110px',
+            marginLeft: '-100px'
+        },
+        bottom_text:{
+            fontFamily: 'filson-pro,sans-serif',
+            fontWeight: '400',
+            fontStyle: 'normal'
         }
     }
 
@@ -53,8 +100,8 @@ const About = () => {
                 <h3 style={styles.title}>¿QUIENES SOMOS?</h3>
             </Row>
             <Row style={{position: 'relative'}}>
-                <p style={styles.top_left_text_1}>holaaaaaaa asd sad as aaaaaaaaad wa a aaaaaaaaaaas ssssss dxxxxxxxxxc xc xxxxxx</p>
-                <p style={styles.top_left_text_2}>aaaaaaa as asaaaaaaf fffffff</p>
+                <p style={styles.top_left_text_1}>{t('about.top.left_1')}</p>
+                <p style={styles.top_left_text_2}>{t('about.top.left_2')}</p>
                 <Col xs={5}><img src={top_left} style={{width: '130%'}}/></Col>
                 <Col xs={7}><img src={top_right} style={styles.top_right}/></Col>
                 <Col xs={12}><img src={top_bottom} style={{marginTop: '-60px'}}/></Col>
@@ -65,16 +112,29 @@ const About = () => {
                 </Col>
             </Row>
             <Row className={'text-center'}>
-                <Col xs={{span:10, offset:1}}><p>asa das dasd awd awd awd w</p></Col>
+                <Col xs={{span:8, offset:2}} className={'mt-5'}><p style={styles.middle_top_text}>{t('about.middle')}</p></Col>
             </Row>
             <Row className={'text-center'}>
                 <Col xs={{span:12}}><img src={mid_top}/></Col>
             </Row>
-            <Row className={'text-center'}>
-                <Col xs={{span:12}}><img style={{marginTop: '-100px'}} src={mid_bottom}/></Col>
+            <Row className={'text-center'} style={{position: 'relative'}}>
+                <Row className={'text-center'} style={styles.middle_bottom_socials_row}>
+                    <Col xs={{span:10, offset:1}}>
+                        <Row>
+                            <Col><a href='https://www.youtube.com/channel/UCeqlCxCxGo1cm1iBZtamgWA' target="_blank" rel="noreferrer" ><img style={styles.icon} src={youtube} /></a></Col>
+                            <Col><a href='https://www.instagram.com/generacionsemilla/' target="_blank" rel="noreferrer"><img style={styles.icon} src={instagram} /></a></Col>
+                            <Col><a href='https://www.facebook.com/generacionsemillaeditorial' target="_blank" rel="noreferrer"><img style={styles.icon} src={facebook} /></a></Col>
+                            <Col><a href='https://www.linkedin.com/company/generaci%C3%B3n-semilla' target="_blank" rel="noreferrer"><img style={styles.icon} src={linkedin} /></a></Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row style={styles.middle_bottom_text_row}>
+                    <Col xs={{span:8, offset:2}}>{t('about.bottom.text_top')}</Col>
+                </Row>
+                <Col xs={{span:12}}><img style={styles.middle_bottom_image} src={mid_bottom}/></Col>
             </Row>
             <Row className={'text-center'}>
-                <Col xs={{span:10, offset:1}}><p>aaaaaaaaa</p></Col>
+                <Col xs={{span:8, offset:2}}><p style={styles.bottom_text}>{t('about.bottom.text_bottom')}</p></Col>
             </Row>
             <Row className={'text-center'}>
                 <Col xs={{span:12}}><img style={styles.bottom} src={bottom}/></Col>
--- src/translations/es.js
+++ src/translations/es.js
@@ -6,6 +6,17 @@ export const TRANSLATIONS_ES = {
         "contact": "Contacto",
         "store": "Tienda",
         "products": "Productos"
+    },
+    about:{
+        top:{
+            "left_1": "Generación semilla es una editorial dedicada a la enseñanza de las ciencias naturales, a la cual no le alcanzó con producir libros, sino que además decidió producir objetos con los cuáles las niñas y los niños puedan interactuar para aprender sobre los fenómenos de la naturaleza.",
+            "left_2": "Experimentar, inspirar, descubrir, jugar y enseñar"
+        },
+        "middle":"Nos representa el compromiso y la responsabilidad con el trabajo; la pasión, vocación y el amor por la enseñanza de las ciencias; la curiosidad y las ganas de aprender guían nuestras propuestas, sumado al profesionalismo y al respeto por la carrera docente. También subyacen la sustentabilidad ambiental y el compromiso social.",
+        bottom:{
+            "text_top":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna vulputate consectetur sed suspendisse ultricies et. Condimentum dictum natoque sodales .",
+            "text_bottom":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna vulputate consectetur sed suspendisse ultricies et. Condimentum dictum natoque sodales pellentesque in at. Luctus tortor egestas vestibulum tellus, viverra cras amet accumsan, pulvinar."
+        }
     }
 
 };
