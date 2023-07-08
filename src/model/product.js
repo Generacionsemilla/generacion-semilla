9d68aa77971b70ee449d42aac9730e417c767aa1
Iván <ivansantamans@gmail.com>
2023-07-08 18:33:00 -0300

    adding products and images

--- src/model/product.js
+++ src/model/product.js
@@ -33,11 +33,11 @@ export const PRODUCTS = [
     /* v ******************/
 
     new Product(
-    /* nombre representativo corto, todo en minuscula, sin espacios ni caracteres especiales */"primaria",
-    /* titulo del libro */'Libros para ninxs',
-    /* texto desktop */ 'Textos de ciencias de la naturaleza pensados para niñxs del primer ciclo de la escuela primaria.\nLos contenidos se basan en las prescripciones de los Diseños Curriculares de Nación, Provincia de Buenos Aires y CABA.\nCada libro incluye, además de textos sobre ciencias, actividades del tipo experiencias y experimentos, entrevistas, curiosidades, juegos, relatos sobre historias de la ciencia y diferentes recursos gráficos que colaboran en la comprensión de los contenidos. \nPor otra parte, cada libro es a su vez, un cuaderno de naturalista, porque tiene cierre elástico, hojas en blanco para registrar experiencias, un sobre para guardar recolecciones y una plancha de sticker troquelados.',
-    /* texto mobile arriba */'Textos de ciencias de la naturaleza pensados para niñxs del primer ciclo de la escuela primaria.\nLos contenidos se basan en las prescripciones de los Diseños Curriculares de Nación, Provincia de Buenos Aires y CABA.\nCada libro incluye, además de textos sobre ciencias, actividades del tipo experiencias y experimentos, entrevistas, curiosidades, juegos, relatos sobre historias de la ciencia y diferentes recursos gráficos que colaboran en la comprensión de los contenidos.',
-    /* texto mobile abajo */'Por otra parte, cada libro es a su vez, un cuaderno de naturalista, porque tiene cierre elástico, hojas en blanco para registrar experiencias, un sobre para guardar recolecciones y una plancha de sticker troquelados.',
+    /* nombre representativo corto, todo en minuscula, sin espacios ni caracteres especiales */"curiosa",
+    /* titulo del libro */'LIBRO CURIOSA BOTÁNICA 1',
+    /* texto desktop */ 'Libro de ciencias de la naturaleza para la infancia\nTrabaja con temas de botánica prescriptos en los diseños curriculares de primero y segundo grado de primaria, como las partes de las plantas, sus características, sus propiedades comestibles y medicinales.\nTiene historias de las ciencias, juegos, experiencias y experimentos y una entrevista a una bióloga botánica',
+    /* texto mobile arriba */'Libro de ciencias de la naturaleza para la infancia\nTrabaja con temas de botánica prescriptos en los diseños curriculares de primero y segundo grado de primaria, como las partes de las plantas, sus características, sus propiedades comestibles y medicinales.\n',
+    /* texto mobile abajo */'Tiene historias de las ciencias, juegos, experiencias y experimentos y una entrevista a una bióloga botánica',
     /* link tienda (boton comprar) */'https://tiendageneracionsemilla.ar/MLA-1137619555-libro-curiosa-botanica-1-botanica-para-ninxs-_JM',
     /* formato imagenes arriba (mobiles y desktop) */ '.png',
     /* formato imagenes abajo (mobiles y desktop) */'.png',
@@ -46,7 +46,28 @@ export const PRODUCTS = [
     /* ^ ***********/
     /* | hasta acá */
     /***************/
-
+    new Product(
+        "mandioca",
+        'LIBRO EL JUEGO DE LA MANDIOCA',
+        'El juego de la mandioca es un libro que contiene un juego de mesa para aprender sobre las plantas de un barrio ficticio:\nEl maravilloso y peculiar barrio de la Mandioca.\nEn el libro encuentran la historia del barrio, las instrucciones y normas del juego, las piezas para recortar como cartas, dado, fichas y una mandioca para armar.\nEn la contratapa está el tablero desplegable y un sobre para guardar todas las piezas del juego.',
+        'El juego de la mandioca es un libro que contiene un juego de mesa para aprender sobre las plantas de un barrio ficticio:\nEl maravilloso y peculiar barrio de la Mandioca.\nEn el libro encuentran la historia del barrio, las instrucciones y normas del juego, las piezas para recortar como cartas, dado, fichas y una mandioca para armar.',
+        'En la contratapa está el tablero desplegable y un sobre para guardar todas las piezas del juego.',
+        'https://www.tiendageneracionsemilla.ar/MLA-1372663157-el-juego-de-la-mandioca-un-libro-para-aprender-botanica-_JM',
+        '.png',
+        '.png',
+        true),
+    
+    new Product(
+        "rita",
+        'LIBRO EN EL PATIO DE MI ABUELA RITA',
+        'En este libro, una niña relata sus encuentros con la abuela en un diario personal durante diez días.\nCada día revive momentos lúdicos con ella, sustentado en la observación, en preguntas y en respuestas que contribuyen al conocimiento sobre aspectos de la biodiversidad en ese pequeño hábitat del patio de su querida abuela Rita.',
+        'En este libro, una niña relata sus encuentros con la abuela en un diario personal durante diez días.',
+        'Cada día revive momentos lúdicos con ella, sustentado en la observación, en preguntas y en respuestas que contribuyen al conocimiento sobre aspectos de la biodiversidad en ese pequeño hábitat del patio de su querida abuela Rita.',
+        'https://www.tiendageneracionsemilla.ar/MLA-1430480616-en-el-patio-de-mi-abuela-rita-libro-sobre-biodiversidad-_JM',
+        '.png',
+        '.png',
+        true),    
+    
     new Product(
         "docente",
         'Libros para Docentes',
